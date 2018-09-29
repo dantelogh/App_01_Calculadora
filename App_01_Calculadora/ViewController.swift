89abe31ead504bc451ff82065376820f4c306dbc
user143004 <user143004@87-237-62-4.rdns.melbourne.co.uk>
2018-09-29 11:02:02 -0700

    Initial Commit

---
+++ App_01_Calculadora/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  App_01_Calculadora
+//
+//  Created by user143004 on 9/29/18.
+//  Copyright © 2018 user143004. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ App_01_CalculadoraTests/App_01_CalculadoraTests.swift
@@ -0,0 +1,34 @@
+//
+//  App_01_CalculadoraTests.swift
+//  App_01_CalculadoraTests
+//
+//  Created by user143004 on 9/29/18.
+//  Copyright © 2018 user143004. All rights reserved.
+//
+
+import XCTest
+@testable import App_01_Calculadora
+
+class App_01_CalculadoraTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
