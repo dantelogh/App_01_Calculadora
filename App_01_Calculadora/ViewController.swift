ae687a50e03d7386a3e967833bed7850745b9286
user143004 <user143004@87-237-62-4.rdns.melbourne.co.uk>
2018-10-02 09:17:43 -0700

    Primeros cambios

--- App_01_Calculadora/ViewController.swift
+++ App_01_Calculadora/ViewController.swift
@@ -9,6 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var n1:Double = 0.0
+    var n2:Double = 0.0
+    var res:Double = 0.0
+
+    @IBOutlet weak var UITextFieldN1: UITextField!
+    @IBOutlet weak var UITextFieldN2: UITextField!
+    @IBOutlet weak var UILabelResultado: UILabel!
+
+    @IBAction func UIButtonSumar(_ sender: Any) {
+        print("Sumar")
+        n1 = Double(UITextFieldN1.text!) ?? 0.0
+        n2 = Double(UITextFieldN2.text!) ?? 0.0
+        res = n1 + n2
+        UILabelResultado.text = String(res)
+    }
+
+    @IBAction func UIButtonRestar(_ sender: Any) {
+        print("Restar")
+        n1 = Double(UITextFieldN1.text!) ?? 0.0
+        n2 = Double(UITextFieldN2.text!) ?? 0.0
+        res = n1 - n2
+        UILabelResultado.text = String(res)
+    }
+    @IBAction func UIButtonMultiplicar(_ sender: Any) {
+        print("Multiplicar")
+        n1 = Double(UITextFieldN1.text!) ?? 0.0
+        n2 = Double(UITextFieldN2.text!) ?? 0.0
+        res = n1 * n2
+        UILabelResultado.text = String(res)
+    }
+    
+    @IBAction func UIButtonDividir(_ sender: Any) {
+        print("Dividir")
+        n1 = Double(UITextFieldN1.text!) ?? 0.0
+        n2 = Double(UITextFieldN2.text!) ?? 0.0
+        res = n1 / n2
+        UILabelResultado.text = String(res)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
